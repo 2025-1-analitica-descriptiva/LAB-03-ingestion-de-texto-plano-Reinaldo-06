bbf0d9ec05a80a2f2c9d88be7613fd03c6dc9040
Reinaldo Lopez <rlopezn@unal.edu.co>
2025-06-12 18:48:40 -0500

    aaaa

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -18,3 +18,17 @@ def pregunta_01():
 
 
     """
+    import pandas as pd
+
+    # Cargar el archivo de texto en un DataFrame
+    df = pd.read_csv('files/input/clusters_report.txt', sep='\t')
+
+    # Renombrar las columnas a minúsculas y reemplazar espacios por guiones bajos
+    df.columns = df.columns.str.lower().str.replace(' ', '_')
+
+    # Separar las palabras clave por coma y un espacio
+    df['palabras_clave'] = df['palabras_clave'].str.replace(', ', ',')
+
+    return df
+
+print(pregunta_01().head())
